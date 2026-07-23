05fcb371eea30176eb15851e8c485495169a7c85
antoniomucollari <mucollariantonio@gmail.com>
2026-07-23 11:56:25 +0200

    - no comment

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -15,8 +15,7 @@ export default defineConfig({
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'Load Test', link: '/backend/load-testing' },
-      { text: 'Examples', link: '/markdown-examples' }
+      { text: 'Load Test', link: '/backend/load-testing' }
     ],
 
     sidebar: [
